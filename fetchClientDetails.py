c7b491822eb8615d70cb49a187645bf868e4877e
Shakti Kumar <shakti.shrivastava13@gmail.com>
2017-07-26 17:12:31 +0530

    6th stage Deployment
    Completed documentation of all files

--- fetchClientDetails.py
+++ fetchClientDetails.py
@@ -1,47 +1,53 @@
 import sys
-'''
-#debugging lines start
-class Dummy:
-    def __init__(self):
-        self.packages = []
-        self.services = []
-        self.msis = []
-#debugging line ends
-'''
     
 class Package:
+    '''
+    objects of class Package will hold information about
+    the packages to be deployed on the client systems
+    '''
     def __init__(self):
-        self.src = None
-        self.destn = None
-        self.name = None
+        self.src = None     #path of the package on the server
+        self.destn = None   #destined path of the package on the client system
+        self.name = None    #a name for the package
 
 class Service:
+    '''
+    objects of class Service will hold information about
+    the services to be configured on the client systems
+    '''
     def __init__(self):
-        self.name = None
-        self.action = None
+        self.name = None    #name of the service
+        self.action = None  #whether to start or stop the service
 
 class MSI:
+    '''
+    objects of class MSI will hold information about
+    the msis to be installed on the client systems
+    '''
     def __init__(self):
-        self.path = None
+        self.path = None    #path of the msi on the server system
 
-#original line
 def fetchClientDetails(obj, configFileData, clientIP, logFile):
-
-    '''    
-#debugging line starts
-if __name__ == '__main__':
-    configFile = r'C:\Users\39232\Desktop\Project Files\configFileFinal.json'
-    obj = Dummy()
-    configFileData = parsingFromDisk(r"C:\Users\39232\Desktop\Project Files\configFileFinal.json")
-    clientIP = '1.1.1.1'
-    #debugging lines end
+    '''
+    This function reads the configFileData which is the dictionary returned by
+    parsingFromDisk(...) function, and constructs the lists <Client_object>.packages,
+    <Client_object>.services and <Client_object>.msis respectively.
+    <Client_object>.packages is a list of objects of class Package, containing information
+    about all the package to be deployed on the system refered to be <Client_object>
+    Similar explanation holds for <Client_object>.services and <Client_object>.msis
+    fetchClientDetails(...) returns, in addition to the Client object, the IP address of
+    the username and password of the refered client
     '''
     
     try:
         thisSystem = configFileData[clientIP]
+        #recall the structure of mapping from parsingMain.py. The data in configFileData is hence
+        #keyed by the IP addresses of the different client system
+        
     except:
         logFile.write('\n'+clientIP+' not found in clients, program exiting')
         print (clientIP,' not found in clients')
+        #in case the clientIP passed to fetchClientDetails(...) doesn't exist in the configFileData
         sys.exit()
         
     try:  
@@ -49,12 +55,16 @@ if __name__ == '__main__':
         
         listofPackages = thisSystem['packages']
         for pkg in listofPackages:
+                #create an object of class Package for each of the packages for 
+                #the client with IP = clientIP
                 p = Package()
                 p.src = str(pkg['source'])
                 p.destn = str(pkg['destn'])
                 p.name = str(pkg['name'])
+                #append this object to the list obj.packages
                 obj.packages.append(p)
         
+        #similarly, create lists obj.services and obj.msis
         listofServices = thisSystem['services']
         for service in listofServices:
                 s = Service()
@@ -86,12 +96,8 @@ if __name__ == '__main__':
         print('Client details fetched\n')
         logFile.write('\nClient details fetched')
         
-        
-        #debugging line
-        #print obj, clientIP, thisSystem['username'], thisSystem['password']
-        
-        #original line
         return obj, thisSystem['username'], thisSystem['password']
+    
     except Exception as e:
         print('Error while fetching client details.\n',str(e))    
         logFile.write('\nError while fetching client details.\n'+str(e))
